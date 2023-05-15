6093a1332814b5d46bc3779be1a468c2a6febbe5
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2023-05-15 18:06:49 +0200

    code for network attributes

--- utils.py
+++ utils.py
@@ -5,7 +5,9 @@ from os.path import isdir, join
 from os import listdir
 import networkx as nx
 from typing import Dict, List
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(rc={'figure.max_open_warning': 0})
 
 def get_all_cities_in_directory(directory:str)->List[str]:
     if(isdir(directory)):
@@ -38,27 +40,36 @@ def create_city_graph(city_name:str, directory:str) -> nx.Graph:
     
     node_info_df = pd.read_csv(join(directory, city_name, "network_nodes_labeled.csv"), sep=",").drop(["lat","lon"],axis=1)
 
-    # Create an empty graph
-    graph = nx.Graph()
 
     # Add edges from the edge list DataFrame to the graph with attributes
-    edges = edge_list_df[['source', 'target']].values
-    edge_attributes = edge_list_df.drop(['source', 'target'], axis=1).to_dict('index')
-    edge_attributes= {str(k): v for k, v in edge_attributes.items()}
     graph = nx.from_pandas_edgelist(edge_list_df, 'source', 'target', True)
 
     #print(graph.get_edge_data(10924,10920))
 
     # Add node information from the node info DataFrame to the graph
-    node_info = node_info_df.set_index('stop_I').to_dict('index')
-    nx.set_node_attributes(graph, node_info)
-    return  graph
+    node_info = node_info_df.set_index('stop_I')
+    nx.set_node_attributes(graph, node_info.to_dict("index"))
+    return  graph, node_info
 
 def get_all_city_graph(directory: str)-> Dict[str, nx.Graph]:
     all_graphs = []
+    all_nodes = []
     directories = get_all_cities_in_directory(directory)
 
     for city in directories:
-        all_graphs.append(create_city_graph(city_name=city, directory=directory))
+        graph, nodes = create_city_graph(city_name=city, directory=directory)
+        nodes["city"] = nodes.apply(lambda x: city,axis=1)
+        all_graphs.append(graph)
+        all_nodes.append(nodes)
 
-    return all_graphs
+    return all_graphs, all_nodes,directories
+
+def add_attribute_to_name(df, attribute_name, fct,graph):
+    dico_to_value = fct(graph)
+    df[attribute_name] = df.apply(lambda x: dico_to_value[x.name],axis=1)
+    return df
+
+
+
+
+    
